3ca70fa49ed113a283401a26afc6e0da0250fad6
ThanhDat-612 <datviproo6a5@gmail.com>
2025-10-26 21:44:03 +0700

    srtf.py

--- srtf.py
+++ srtf.py
@@ -1,55 +1,65 @@
-import matplotlib.pyplot as plt
+# =============== SJF preemptive ================
+# CPU luon chon tien trinh  co thoi gian xu ly (burst time) nho nhat trong cac tien trinh da den (arrival time <= current time)
+# Khi CPU da chon mot tien trinh thi no se xu ly cho den khi hoan tat hoac bi ngat
+import matplotlib.pyplot as plt      # thu vien ve chart
+
 def NhapDuLieu():
-    n = int(input("Nhap so process: "))
-    AT, BT = [], []
+    n = int(input("Nhap so process: "))                    # nhap so tien trinh
+    AT, BT = [], []                                        # tao danh sach ArrivalTime, BurstTime
+    # nhap thoi gian den (at) va thoi gian xu ly(bt) cho tung tien trinh
     for i in range(n):
         at = int(input(f"Arrival Time P{i+1}: "))
         bt = int(input(f"Burst Time P{i+1}: "))
         AT.append(at)
         BT.append(bt)
-    return n, AT, BT
+    return n, AT, BT           # tra ve so tien trinh va danh sach AT, BT
 
+# HAM THUC THI SJF (SRTF)
+def sjfPreemptive(n, AT, BT):
+    completed = [False] * n                # danh dau tien trinh da hoan thanh hay chua
+    CT = [0] * n  # Completion Time        # complete time: thoi gian hoan thanh
+    TAT = [0] * n # Turn Around Time       # TurnAroundTime: thoi gian ma tien trinh o trong he thong = CT - AT
+    WT = [0] * n  # Waiting Time           # WaitingTime: thoi gian cho = TAT - BT
+    RT = BT.copy()                         # RemainingTime = BurstTime 
+    currentTime = 0                        # thoi gian hien tai
+    completedCount = 0                     # so tien trinh hoan thanh
+    order = []                             # dung de luu thu tu tien trinh duoc CPU xu ly
+    startTime,endTime  = [],[]             # luu thoi diem bat dau va ket thuc cho tung buoc xu ly  (dung cho bieu do)
 
-def sjfNonPreemptive(n, AT, BT):
-    completed = [False] * n
-    CT = [0] * n  # Completion Time
-    TAT = [0] * n # Turn Around Time
-    WT = [0] * n  # Waiting Time
-    RT = BT.copy()
-    currentTime = 0
-    completedCount = 0
-    order = []
-    startTime,endTime  = [],[]
+    # chay cho toi khi tat ca tien trinh hoan tat
     while completedCount < n:
-        
-        index = -1
-        minRT = float('inf')
+        index = -1                         # lưu vị trí tien trinh dc chon
+        minRT = float('inf')               # d(minTime): gia tri nho nhat TAM THOI de tim RT nho nhat
 
+        # tim tien trinh san sang (da den va co RT nho nhat)
         for i in range(n):
             if (AT[i] <= currentTime and not completed[i] and RT[i]<minRT):
                 minRT = RT[i]
                 index = i
-
+                
+        # Neu co P san sang thi giam thoi gian di 1 don vi
         if index != -1:
-            RT[index] -= 1
-            order.append(f"P{index+1}")
-            startTime.append(currentTime)
-            endTime.append(currentTime+1)
+            RT[index] -= 1 
+            order.append(f"P{index+1}")       # them tien trinh duoc chay vao thu tu
+            startTime.append(currentTime)     # luu lai thoi gian (dung cho bieu do)
+            endTime.append(currentTime+1)     # luu lai thoi gian (dung cho bieu do)
 
+            # Neu P chay xong
             if(RT[index]==0):
-                completed[index] = True
-                completedCount +=1
-                CT[index] = currentTime+1
+                completed[index] = True        # danh dau hoan thanh
+                completedCount +=1             # tang so luong P hoan thanh len 1
+                CT[index] = currentTime+1      # ghi lai thoi gian hoan thanh cua P do
         else:
-            order.append(f"IDLE")
+            order.append(f"IDLE")              # khong co tien trinh nao san sang
         
-        currentTime +=1
+        currentTime +=1       # tang 1  don vi thoi gian
 
-
+    # tinh toan / dung cho bieu do
     for i in range(n):
         TAT[i] = CT[i] - AT[i]
         WT[i] = TAT[i] - BT[i]
 
+    # tra ket qua ve
     return CT, TAT, WT, order, startTime, endTime
 
 def XuatDuLieu(n, AT, BT, CT, TAT, WT, order):
@@ -59,16 +69,18 @@ def XuatDuLieu(n, AT, BT, CT, TAT, WT, order):
 
     print("Excuted order:")
     print(" -> ".join(order))
-
+    
+# HAM VE BIEU DO
 def importGanttChart(order,start,end):
     fig,gnt = plt.subplots()
     gnt.set_xlabel("TIME")
     gnt.set_ylabel("PROCESS")
     gnt.set_title("Gantt Chart")
 
-    #Tao list cac tien trinh duy nhat
+    #Tao list cac tien trinh duy nhat (bao gom "IDLE")
     processes = list(sorted(set(order)))
 
+    # gan tung vi tri tren truc Y cho moi tien trinh
     y_pos = {p:(i+1)*10 for i,p in enumerate(processes)}
 
     # Draw
